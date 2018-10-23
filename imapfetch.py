ba1119d8473eb382541bcaf60ecc046ec47d7eba
Anton Semjonov <anton@semjonov.de>
2018-10-23 17:20:13 +0200

    add better commenting

--- imapfetch.py
+++ imapfetch.py
@@ -10,17 +10,27 @@ import email
 import os
 import re
 
-logging.basicConfig(level=logging.INFO)
+# TODO: seperate logging per class/folder
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger("imapfetch")
 
+# cryptographic hash for indexing purposes
+Blake2b = lambda b: hashlib.blake2b(b).digest()
 
+# path utilities, join path to absolute and expand ~ home
+path = os.path
+join = lambda p, base=".": path.abspath(path.join(base, path.expanduser(p)))
+
+# Mailserver is a connection helper to an IMAP4 server.
+#! Since most IMAP operations are performed on the currently selected folder
+#! this is absolutely not safe for concurrent use. Weird things will happen.
 class Mailserver:
 
-    # open connection
-    def __init__(self, server, username, password, log=log):
+    # open connection to server, pass imaplib.IMAP4 if you don't want TLS
+    def __init__(self, server, username, password, log=log, IMAP=imaplib.IMAP4_SSL):
         self.log = log
         self.log.info(f"connect to {server} as {username}")
-        self.connection = imaplib.IMAP4_SSL(server)
+        self.connection = IMAP(server)
         self.connection.login(username, password)
 
     # list available folders
@@ -28,39 +38,46 @@ class Mailserver:
         folders = self.connection.list()[1]
         return (re.sub(r"^\([^)]+\)\s\".\"\s", "", f.decode()) for f in folders)
 
-    # "change directory"
+    # "change directory", readonly
     def cd(self, folder):
         self.connection.select(folder, readonly=True)
 
-    # get new mails, starting with uidstart
+    # get new mail uids in current folder, starting with uidstart
     # https://blog.yadutaf.fr/2013/04/12/fetching-all-messages-since-last-check-with-python-imap/
-    def mails(self, uidstart=1):
+    def mails(self, uidstart=0):
         # search for and iterate over message uids
         uids = self.connection.uid("search", None, f"UID {uidstart}:*")[1]
         for uid in uids[0].split():
-            # search always returns at least one result
             if int(uid) > uidstart:
-                # fetch message body
                 yield uid
 
+    # chunk sizes for partial fetches
+    # 64 kB should be enough to fetch all possible headers and small messages in one go
     FIRSTCHUNK = 64 * 1024
+    # for any larger messages use 1 MB chunks
     NEXTCHUNK = 1024 * 1024
 
-    # partial generator for a single mail
-    def partials(self, uid):
+    # partial generator for a single mail, for use with next() or for..in.. statements
+    def partials(self, uid, firstchunk=FIRSTCHUNK, nextchunk=NEXTCHUNK):
         offset = 0
-        chunksize = self.FIRSTCHUNK
+        chunksize = firstchunk
         while True:
-            log.debug(f"partial fetch {uid}: offset={offset} size={chunksize}")
+            # partial fetch using BODY[]<o.c> syntax
+            self.log.debug(f"partial fetch {int(uid)}: offset={offset} size={chunksize}")
             wrap, data = self.connection.uid("fetch", uid, f"BODY[]<{offset}.{chunksize}>")[1][0]
             yield data
+            # check if the chunksize was smaller than requested --> assume no more data
             if int(re.sub(r".*BODY\[\].* {(\d+)}$", r"\1", wrap.decode())) < chunksize:
                 return
-            chunksize = self.NEXTCHUNK
+            chunksize = nextchunk
             offset += chunksize
 
 
+# Maildir is a maildir-based email storage for local on-disk archival.
+# TODO: use plain named subfolders without dot-prefix (similar to thunderbird)
+# TODO: integrate the index dbm directly
 class Maildir:
+
     # open a new maildir mailbox
     def __init__(self, path, log=log):
         self.log = log
@@ -79,12 +96,14 @@ class Maildir:
         return key
 
 
+# Account is a configuration helper, parsing a section from a configuration file
+# and for use in a with..as.. statement.
 class Account:
     # parse account data from configuration section
     def __init__(self, section):
-        self._archive = joinpath(".", section.get("archive"))
-        self._index = joinpath(self._archive, "index")
-        self.incremental = section.getboolean("incremental")
+        self._archive = join(section.get("archive", "./archive"))
+        self._index = join("index", base=self._archive)
+        self.incremental = section.getboolean("incremental", True)
         self._server = section.get("server")
         self._username = section.get("username")
         self._password = section.get("password")
@@ -104,16 +123,6 @@ class Account:
         self.server.connection.logout()
 
 
-# hash message header for indexing
-def digest(header):
-    return hashlib.blake2b(header).digest()
-
-
-# join paths and return absolute
-def joinpath(base, path):
-    return os.path.abspath(os.path.join(base, os.path.expanduser(path)))
-
-
 # -----------------------------------------------------------------------------------
 if __name__ == "__main__":
 
@@ -127,7 +136,7 @@ if __name__ == "__main__":
                 log.info(f"process folder {folder}")
 
                 server.cd(folder)
-                highest = int(index.get(folder, 1))
+                highest = int(index.get(folder, 0))
 
                 for uid in server.mails(highest if acc.incremental else 1):
                     partials = server.partials(uid)
@@ -140,7 +149,7 @@ if __name__ == "__main__":
                     # get digest of header only
                     e = email.message_from_bytes(message)
                     e.set_payload("")
-                    dgst = digest(e.as_bytes())
+                    dgst = Blake2b(e.as_bytes())
 
                     # mail already exists?
                     if dgst in index:
