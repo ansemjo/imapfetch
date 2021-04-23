81366885173508b6f8eb4419da26e7cc40e0fc90
Anton Semjonov <anton@semjonov.de>
2021-04-23 23:37:06 +0200

    refactor logging everywhere
    - re-add VERBOSE between INFO and DEBUG
    - use log(LEVEL, ...) everywhere

--- imapfetch/imapfetch.py
+++ imapfetch/imapfetch.py
@@ -14,17 +14,20 @@ def interrupt(sig, frame):
     sys.exit(0)
 signal.signal(signal.SIGINT, interrupt)
 
+# add a verbose logging level and use keywords
+from logging import ERROR, WARNING, INFO, DEBUG
+VERBOSE = INFO - 5
+
 # Mailserver is a connection helper to an IMAP4 server.
 #! Since most IMAP operations are performed on the currently selected folder
 #! this is absolutely not safe for concurrent use. Weird things *will* happen.
 class Mailserver:
 
     def __init__(self, host, username, password, logger=None):
-        logname = "{}/mailserver".format(logger.name if logger is not None else "imapfetch")
-        self.log = logging.getLogger(logname)
-        self.log.info("connecting to {}".format(host))
+        self.log = logger.log if logger else logging.getLogger("mailserver").log
+        self.log(INFO, "connecting to {}".format(host))
         self.client = imapclient.IMAPClient(host=host, use_uid=True, ssl=True)
-        self.log.info("logging in as {}".format(username))
+        self.log(INFO, "logging in as {}".format(username))
         self.client.login(username, password)
         
     # stubs for use as a context manager
@@ -58,7 +61,7 @@ class Mailserver:
 
     # thin wrapper on imapclient's fetch for debug logging
     def fetch(self, uid, data, modifiers=None):
-        self.log.debug("FETCH {} [{}]".format(uid, data))
+        self.log(VERBOSE, "FETCH {} [{}]".format(uid, data))
         return self.client.fetch(uid, data, modifiers)[uid]
 
     # retrieve a specific message by uid; return header and body generator
@@ -74,7 +77,7 @@ class Mailserver:
             pos = len(text)
             yield header + text
             while size > (len(header) + pos):
-                self.log.debug("next partial: <{}.{}>".format(pos, chunk))
+                self.log(VERBOSE, "next partial: <{}.{}>".format(pos, chunk))
                 part = self.fetch(uid, [self.TEXTF % (pos, chunk)])[self.TEXT % (pos)]
                 pos += len(part)
                 yield part
@@ -153,12 +156,11 @@ class Archive:
 
     # open a new archive
     def __init__(self, path, logger=None, quoting=False):
-        logname = "{}/archive".format(logger.name if logger is not None else "imapfetch")
-        self.log = logging.getLogger(logname)
+        self.log = logger.log if logger else logging.getLogger("archive").log
         self.path = os.path.abspath(os.path.expanduser(path))
         os.makedirs(self.path, exist_ok=True)
         self.index = dbm.open(os.path.join(self.path, "index"), flag="c")
-        self.log.info("opened archive in {}".format(self.path))
+        self.log(INFO, "opened archive in {}".format(self.path))
         self.quoting = quoting
 
     # stubs for use as a context manager
@@ -186,6 +188,7 @@ class Archive:
         if self.quoting:
             folder = urllib.parse.quote_plus(folder)
         folder = folder.replace("/", ".")
+        self.log(VERBOSE, "open mailbox in {!r}".format(folder))
         return Maildir(os.path.join(self.path, folder), create=True)
 
     # archive a message in mailbox
@@ -197,7 +200,7 @@ class Archive:
         inbox = self.inbox(folder)
         file = inbox.add(message, uid)
         self.index[message.digest()] = uid.to_bytes(4, "big")
-        self.log.warning("message uid {} stored in {}".format(uid, file))
+        self.log(INFO, "message uid {} stored in {}".format(uid, file))
         return message.digest()
 
 
@@ -246,65 +249,66 @@ def imapfetch():
     parser.add_argument("section", help="sections to execute", nargs="*")
     parser.add_argument("--full", "-f", help="do full backups", action="store_true")
     parser.add_argument("--list", "-l", help="only list folders", action="store_true")
-    parser.add_argument("--verbose", "-v", help="increase logging verbosity", action="count", default=0)
+    parser.add_argument("--verbose", "-v", help="increase logging verbosity", action="count", default=1)
     args = parser.parse_args()
 
     # configure logging format and verbosity
-    # TODO: add more level between info and debug again?
-    level = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
+    level = [ERROR, WARNING, INFO, VERBOSE, DEBUG]
     level = level[min(len(level) - 1, args.verbose)]
     logging.basicConfig(format="%(name)s: %(message)s", level=level)
-    log = logging.getLogger("imapfetch")
-    log.debug(args)
+    applog = logging.getLogger("imapfetch").log
+    applog(DEBUG, args)
 
     # read configuration
-    log.info("read configuration from {}".format(args.config.name))
+    applog(INFO, "read configuration from {}".format(args.config.name))
     conf = configparser.ConfigParser()
     conf.read_file(args.config)
 
     # check given section names for existence
     for section in args.section:
         if section not in conf.sections():
-            raise ValueError("no such section in configuration: {}".format(section))
+            applog(ERROR, "no such section in configuration: {}".format(section))
+            sys.exit(1)
 
     # iterate over selected configuration sections
     for section in (args.section or conf.sections()):
 
         # create logger
-        log.info("processing section {}".format(section))
-        sectlog = logging.getLogger(section)
+        applog(INFO, "processing section {}".format(section))
+        logger = logging.getLogger(section)
+        log = logger.log
 
         # if --list is given only connect and show folders
         if args.list:
-            with Account(conf[section], sectlog).imap() as mailserver:
-                sectlog.info("listing folders:")
+            with Account(conf[section], logger).imap() as mailserver:
+                log(INFO, "listing folders:")
                 for folder in mailserver.ls():
-                    sectlog.warning(folder)
+                    log(WARNING, folder)
             continue
 
         # otherwise open archive for processing
-        with Account(conf[section], sectlog).ctx() as (acc, mailserver, archive):
+        with Account(conf[section], logger).ctx() as (acc, mailserver, archive):
             for folder in mailserver.ls():
 
                 # test for exclusion matches
                 def checkskip(rules, folder):
                     for pattern in rules:
                         if fnmatch.fnmatch(folder, pattern):
-                            sectlog.info("excluded folder {} due to {!r}".format(folder, pattern))
+                            log(VERBOSE, "excluded folder {} due to {!r}".format(folder, pattern))
                             return True
                 if checkskip(acc.exclude, folder):
                     continue
 
                 # send imap command to change directory 
-                sectlog.warning("processing folder {}".format(folder))
+                log(INFO, "processing folder {}".format(folder))
                 mailserver.cd(folder)
 
                 # retrieve the highest known uid from index
                 uidkey = "{}/{}".format(section, folder)
                 highest = archive.lastseen(uidkey)
-                sectlog.debug("lastseen uid for {} = {}".format(uidkey, highest))
+                log(VERBOSE, "lastseen uid for {} = {}".format(uidkey, highest))
                 if args.full:
-                    sectlog.info("starting at uid 1")
+                    log(INFO, "starting at uid 1")
 
                 # iterate over all uids >= highest
                 for uid in mailserver.mails(1 if args.full else highest):
@@ -312,7 +316,7 @@ def imapfetch():
                     
                     # check if the email is stored already
                     if header in archive:
-                        sectlog.info("message uid {} stored already".format(uid))
+                        log(VERBOSE, "message uid {} stored already".format(uid))
                     else:
                         # otherwise collect full message and store
                         message = b"".join(generator())
